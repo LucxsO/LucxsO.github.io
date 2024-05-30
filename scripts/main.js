a0e88efe2a300f0f9205c9f427db7f2e7e90b3c4
LucxsO <144711048+LucxsO@users.noreply.github.com>
2024-05-30 16:26:44 +0000

    reset

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Highlight the active link
     const navLinks = document.querySelectorAll('.animated-nav-menu a');
-    const slider = document.querySelector('.slider');
-
-    const updateSlider = (activeLink) => {
-        const activeRect = activeLink.getBoundingClientRect();
-        const navRect = activeLink.closest('ul').getBoundingClientRect();
-        slider.style.left = `${activeRect.left - navRect.left}px`;
-        slider.style.width = `${activeRect.width}px`;
-    };
-
     navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            navLinks.forEach(link => link.classList.remove('active'));
-            link.classList.add('active');
-            updateSlider(link);
-        });
-
         if (link.href === window.location.href) {
             link.classList.add('active');
-            updateSlider(link);
-        }
-    });
-
-    window.addEventListener('resize', () => {
-        const activeLink = document.querySelector('.animated-nav-menu a.active');
-        if (activeLink) {
-            updateSlider(activeLink);
         }
     });
 });
